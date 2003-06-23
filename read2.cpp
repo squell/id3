bf63c8543a0d21a9b01a27154972255670f2f894
Marc Schoolderman <squell@xs4all.nl>
2003-06-23 07:15:04 +0000

    some more mods

--- read2.cpp
+++ read2.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <iostream>
+#include <string>
 #include "id3v2.h"
 
 class ID3tag {
@@ -21,22 +23,36 @@ private:
 class ID3tag::iterator {
     friend class ID3tag;
 public:
+    class frame {
+        friend class iterator;
+        ID3FRAME f;
+    public:
+        int tag_volit()    const { return f->tag_volit;  }
+        int file_volit()   const { return f->file_volit; }
+        int readonly()     const { return f->readonly;   }
+        int packed()       const { return f->packed;     }
+        int encrypted()    const { return f->encrypted;  }
+        int grouped()      const { return f->grouped;    }
+        const char* ID()   const { return f->ID;         }
+        std::string data() const { return std::string(f->data, f->size); }
+    };
+
     iterator() : stat(false) { }
 
     iterator& operator++()
-    { stat = ID3_frame(f); return *this; }
+    { stat = ID3_frame(f.f); return *this; }
 
     iterator  operator++(int)
     { iterator t = *this; ++(*this); return t; }
 
-    const _ID3FRAME& operator*() const
-    { return *f; }
+    const frame& operator*() const
+    { return frm; }
 
-    const _ID3FRAME* operator->() const
-    { return f; }
+    const frame* operator->() const
+    { return &frm; }
 
     bool operator==(const iterator& other) const
-    { return stat == other.stat && (!stat || f->data == other->data); }
+    { return stat == other.stat && (!stat || f.f->data == other.f.f->data); }
 
     bool operator!=(const iterator& other) const
     { return !(*this == other); }
@@ -45,11 +61,11 @@ public:
     { return stat; }
 
 private:
-    ID3FRAME f;
+    frame f;
     bool stat;
 
     iterator(void* buf)
-    { ID3_start(f, buf); stat = ID3_frame(f); }
+    { ID3_start(f.f, buf); stat = ID3_frame(f.f); }
 };
 
 inline ID3tag::iterator ID3tag::begin()
@@ -77,6 +93,7 @@ int main(int argc, char *argv[])
     ID3tag tag(argv[1]);
 
     for(ID3tag::iterator p = tag.begin(); p != tag.end(); p++)
-        std::printf("%s\n\t%.*s\n", p->ID, (int)p->size-1, p->data+1);
+//      std::printf("%s\n\t%.*s\n", p->ID, (int)p->size-1, p->data+1);
+        cout << p->ID() << endl << p->data() << endl;
 }
 
